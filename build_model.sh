9966dfbdf2c063703203090b4140e6c8ea66008a
wscho <wscho@snu.ac.kr>
2018-06-25 13:32:03 +0900

    dictionary src & lib builder for a specific model

---
+++ build_model.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $1 == "" ]]
+then
+	echo " * -------------------------------- *"
+	echo " * OptiMass model src & lib builder *"
+	echo " * -------------------------------- *"
+	echo " Usage: $./build_model.sh <your_model.xml>"
+	echo " "
+	exit -1;
+fi
+
+modelfile=$1
+modelcard="./model/model_card.xml"
+
+if [ -f $modelfile ]
+then
+	echo ""
+    echo " *processing ${modelfile}..."
+    cp $modelfile $modelcard;
+    ./build_model_dictionary;
+    make model;
+else
+    "There's no modelfile named ${modelfile}"
+fi
+
+exit 0;
+
